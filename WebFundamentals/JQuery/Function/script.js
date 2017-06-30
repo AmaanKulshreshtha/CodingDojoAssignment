0ad5c1e35b998941a6b393a84eaaaab1668fa0cf
SUN Edward <edward.sun@leica-geosystems.com>
2017-06-29 18:41:36 -0700

    commit twice

--- WebFundamentals/JQuery/Function/script.js
+++ WebFundamentals/JQuery/Function/script.js
@@ -5,23 +5,71 @@ $(document).ready(function()
   });
 
   $('#toggle').click(function(){
-    $('.toggle > img').slideToggle(2000,
+    $('.toggle > img').toggle(2000,
         function(){
-           alert("image toggle complete!");
+           alert("toggle complete!");
         });
   });
-  
-  $('img').click(function(){
-    console.log('data-alt-src value is',$(this).attr('data-alt-src'));
-    var alt_src = $(this).attr('data-alt-src');  //getter retrieve relative path of cat0
-    var src = $(this).attr('src');
-    console.log('src= '+src);
-    $(this).attr('src',alt_src);      //trick to swap src attribute with data-alt-src
-    $(this).attr('data-alt-src',src);
 
+  $('#slideDown').click(function(){
+     $('.toggle > img').slideDown(1000,function(){
+       alert("Slided down!");
+     })
   });
 
+  $('#slideUp').click(function(){
+     $('.toggle > img').slideUp(1000,function(){
+       alert("Slided up!");
+     })
+  });
 
+  $('#slideToggle').click(function(){
+    $('.toggle > img').slideToggle(2000,
+        function(){
+           alert("Slide toggle complete!");
+        });
+  });
+
+  $('#fadeIn').click(function(){    /*in css, we use display: none to let the div invisible
+    so we can see fadeIn affect*/
+    console.log("clicked fadeIn button.");
+    $('.fade').fadeIn("slow",
+        function(){
+           alert("Fade In complete!");
+        });
+  });
+
+  $('#fadeOut').click(function(){
+    console.log("clicked fadeOut button.");
+    $('.fadeOut > p').fadeOut("slow",
+        function(){
+            alert("Fade Out complete!");
+        });
+  });
+
+  $('#before').click(function(){
+    console.log("clicked before button.");
+    $('.before > p').before("<h1>H1 Title added before paragraph</h1>");
+  });
+
+  $('#before').click(function(){
+    console.log("clicked before button.");
+    $('.before > p').before("<h1>H1 Title added before paragraph</h1>");
+  });
+
+  $('#after').click(function(){
+    console.log("clicked after button.");
+    $('.after > p').after("<h3>H3 Title added after paragraph</h3>");
+  });
+
+  $('#getHTML').click(function(){
+    var htmlString = $('.content > p').html();
+    $('.content > p').after("<h3>htmlString</h3>")
+  });
+
+  $('#value').click(function(){
+    console.log($('#box1').val());
+  });
   //  alert('Page fully loaded!');
    //
   //  $('a').click(function(){
