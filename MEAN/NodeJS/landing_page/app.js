f0f7c04a7760a9c25d5f744c195dc81d29ff26d3
blackhatSolo <edwardsun007@gmail.com>
2017-11-18 20:40:17 -0800

    NodeJS commit

---
+++ MEAN/NodeJS/landing_page/app.js
@@ -0,0 +1,52 @@
+var http = require('http');
+
+//fs module allows us to read and write content for responses!!
+var fs = require('fs');
+//creating a server using http module:
+var server = http.createServer(function (request,response){
+    console.log('client request URL:',request.url);
+    // this is how we do routing:
+    if(request.url==='/'){
+      //
+      fs.readFile('./index.html','utf8',function(errors,contents){
+        console.log(typeof(contents));
+        console.log('passed begin');
+        response.writeHead(200,{'Content-Type':'text/html'}); //send data about response
+        console.log('passed writeHead');
+        response.write(contents);//send response body
+        console.log('passed writeContents');
+        response.end();//finished
+      });
+    }
+    else if(request.url==='/ninjas.html'){ // we need to do this because
+      // the style.css is actually stored on our server
+      fs.readFile('./ninjas.html','utf8',function(errors,contents){
+        console.log('started processing ninjas...');
+        response.writeHead(200,{'Content-Type':'text/html'});
+        console.log('passed write head ninjas');
+        response.write(contents);
+        response.end();
+      });
+    }
+    else if(request.url==='/dojos.html'){ // we need to do this because
+      // the style.css is actually stored on our server
+      fs.readFile('./dojos.html','utf8',function(errors,contents){
+        console.log('started processing dojos...');
+        response.writeHead(200,{'Content-Type':'text/html'});
+        console.log('passed writeHead dojos');
+        response.write(contents);
+        response.end();
+      });
+    }
+
+// request did match anything
+    else{
+      response.writeHead('404');
+      response.end('File not found!!!');
+    }
+});
+
+// tell your server which port to run on
+server.listen(6789);
+// print to terminal window
+console.log("Running in localhost at port 6789");
---
+++ MEAN/NodeJS/test.js
@@ -0,0 +1,2 @@
+console.log("Test run this script.");
+console.log("Making some changes.");
